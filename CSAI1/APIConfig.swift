6416cda884ec6240318fe4b0ae3508317d7791c4
DrVanus <danielm188@aol.com>
2025-03-31 02:48:28 -0400

    Add files via upload

---
+++ CSAI1/APIConfig.swift
@@ -0,0 +1,17 @@
+//
+//  APIConfig.swift
+//  CSAI1
+//
+//  Created by DM on 3/24/25.
+//
+
+
+//
+//  APIConfig.swift
+//
+//  IMPORTANT: Do not commit this file with your actual API key to your public repository.
+//
+struct APIConfig {
+    // Replace the below string with your testing OpenAI API key.
+    static let openAIKey = "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"
+}
